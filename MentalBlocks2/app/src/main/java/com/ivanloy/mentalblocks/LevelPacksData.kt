572f7955c1949d5368ec7398af440f2970277ba5
Ignacio Van Loy <nachovanle@gmail.com>
2019-02-14 12:46:38 +0100

    Building Level Pack enum

--- MentalBlocks2/app/src/main/java/com/ivanloy/mentalblocks/LevelPacksData.kt
+++ MentalBlocks2/app/src/main/java/com/ivanloy/mentalblocks/LevelPacksData.kt
@@ -2,6 +2,8 @@ package com.ivanloy.mentalblocks
 
 enum class LevelPacksData(val levels : Array<LevelsData>) {
 
-
+    TUTORIAL(arrayOf(LevelsData.LEVEL_1, LevelsData.LEVEL_2, LevelsData.LEVEL_3, LevelsData.LEVEL_4,
+        LevelsData.LEVEL_5, LevelsData.LEVEL_6, LevelsData.LEVEL_7, LevelsData.LEVEL_8, LevelsData.LEVEL_9,
+        LevelsData.LEVEL_10, LevelsData.LEVEL_11, LevelsData.LEVEL_12))
 
 }
